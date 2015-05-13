dd4b37e20d7f1e0b75a1e7dc4e8b216fdb179db3
Przemek <przemek@krowinski.eu>
2015-05-13 23:14:41 +0200

    Add missing requires

--- lib/arch_presenter.rb
+++ lib/arch_presenter.rb
@@ -1,3 +1,5 @@
 require 'arch_presenter/version'
 require 'arch_presenter/base'
+require 'arch_presenter/action_view/helpers'
+require 'arch_presenter/action_controller/methods'
 require 'arch_presenter/railtie' if defined?(Rails)
