5f6f249d36d22a6e1ba3e50cdc938bc4beb14c20
Przemek <przemek@krowinski.eu>
2015-05-13 23:06:55 +0200

    Fix invalid module name

--- lib/arch_presenter/railtie.rb
+++ lib/arch_presenter/railtie.rb
@@ -2,11 +2,11 @@ module ArchPresenter
   class Railtie < ::Rails::Railtie
     initializer "my_gem.configure_view_controller" do |app|
       ActiveSupport.on_load :action_view do
-        include MyPresenter::ActionView::Helpers
+        include ArchPresenter::ActionView::Helpers
       end
 
       ActiveSupport.on_load :action_controller do
-        include MyPresenter::ActionController::Methods
+        include ArchPresenter::ActionController::Methods
       end
     end
   end
