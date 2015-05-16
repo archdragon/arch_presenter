f73b66b25a7353c1fec5adff7f007608ae009cb5
Przemek <przemek@krowinski.eu>
2015-05-16 20:27:18 +0200

    Move #present to Presenter class

--- lib/arch_presenter.rb
+++ lib/arch_presenter.rb
@@ -1,5 +1,6 @@
 require 'arch_presenter/version'
 require 'arch_presenter/base'
+require 'arch_presenter/presenter'
 require 'arch_presenter/action_view/helpers'
 require 'arch_presenter/action_controller/methods'
 require 'arch_presenter/railtie' if defined?(Rails)
