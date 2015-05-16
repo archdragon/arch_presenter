64c7fd3a760dc387604fbb141349e2572f67f46d
Przemek <przemek@krowinski.eu>
2015-05-16 20:37:23 +0200

    Update class names

--- lib/arch_presenter/action_controller/methods.rb
+++ lib/arch_presenter/action_controller/methods.rb
@@ -2,7 +2,7 @@ module ArchPresenter
   module ActionController
     module Methods
       def present(target_object)
-        Base.present(target_object)
+        Presenter.present(target_object)
       end
     end
   end
--- lib/arch_presenter/action_view/helpers.rb
+++ lib/arch_presenter/action_view/helpers.rb
@@ -2,7 +2,7 @@ module ArchPresenter
   module ActionView
     module Helpers
       def present(target_object)
-        Base.present(target_object)
+        Presenter.present(target_object)
       end
     end
   end
