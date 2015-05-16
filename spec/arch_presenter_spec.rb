540f442605091760c3ee917fac812714a43f1c57
Przemek <przemek@krowinski.eu>
2015-05-16 20:30:18 +0200

    Move Base test to Pesenter
    Move Base test after extracting presenting logic from Base to Presenter

--- spec/arch_presenter_spec.rb
+++ spec/arch_presenter_spec.rb
@@ -1,32 +1,7 @@
 require 'spec_helper'
 
-class TestThing; end
-
-class NoMatchingPresenter; end
-
-class TestThingPresenter < SimpleDelegator
-  def name
-    'Presented Test Thing'
-  end
-end
-
 describe ArchPresenter do
   it 'has a version number' do
     expect(ArchPresenter::VERSION).not_to be nil
-  end
-
-  context 'when presenter exists' do
-    it 'it adds presenter methods to the object' do
-      test_thing = TestThing.new
-      test_thing = ArchPresenter::Base.present(test_thing)
-      expect(test_thing.name).to be == 'Presented Test Thing'
-    end
-  end
-  context 'when presenter doesn\'t exist' do
-    it 'returns the source object' do
-      class_wihout_presenter = NoMatchingPresenter.new
-      class_wihout_presenter = ArchPresenter::Base.present(class_wihout_presenter)
-      expect(class_wihout_presenter.class).to be == NoMatchingPresenter
-    end
   end
 end
