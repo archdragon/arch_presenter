051bf7377ec95f99f8b46b78a883f06b98c1bf2e
Przemek <przemek@krowinski.eu>
2015-05-16 20:29:53 +0200

    Extract presenting logic to a new class

---
+++ lib/arch_presenter/presenter.rb
@@ -0,0 +1,11 @@
+module ArchPresenter
+  class Presenter
+    def self.present(target_object)
+      presenter_class_name = "#{target_object.class}Presenter"
+      class_constant = presenter_class_name.constantize
+      class_constant.new(target_object)
+    rescue NameError
+      target_object
+    end
+  end
+end
