1481112a91cf9484cf75044339f0f560d3817309
Przemek <przemek@krowinski.eu>
2015-05-25 15:50:27 +0200

    Add a comment related to the exception

--- lib/arch_presenter/presenter.rb
+++ lib/arch_presenter/presenter.rb
@@ -4,6 +4,7 @@ module ArchPresenter
       presenter_class_name = "#{target_object.class}Presenter"
       class_constant = presenter_class_name.constantize
       class_constant.new(target_object)
+    # If the presenter class doen't exist resturn the original object
     rescue NameError
       target_object
     end
