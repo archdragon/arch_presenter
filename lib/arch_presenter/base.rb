2f7689029f06f5be30df6a36e711b8ba2b2f815b
Przemek <przemek@krowinski.eu>
2015-05-25 13:58:11 +0200

    Remove unused methods (original_class_name, etc)

--- lib/arch_presenter/base.rb
+++ lib/arch_presenter/base.rb
@@ -1,17 +1,4 @@
 module ArchPresenter
   class Base < ::SimpleDelegator
-    def self.human_attribute_name(*args)
-      original_class.human_attribute_name(*args)
-    end
-
-    def original_class
-      presenter_class_name.chomp('Presenter').constantize
-    end
-
-    private
-
-    def presenter_class_name
-      self.class.to_s
-    end
   end
 end
--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -1,21 +1,4 @@
 require_relative 'spec_helper'
 
-class TestThing; end
-
-class NoMatchingPresenter; end
-
-class TestThingPresenter < ArchPresenter::Base
-  def name
-    'Presented Test Thing'
-  end
-end
-
 describe ArchPresenter::Base do
-  describe '#original_class' do
-    it 'returns the class that current presenter was based on' do
-      test_thing = TestThing.new
-      test_thing = ArchPresenter::Presenter.present(test_thing)
-      expect(test_thing.original_class).to be == TestThing
-    end
-  end
 end
